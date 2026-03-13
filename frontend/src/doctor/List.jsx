b6961bd8ddd05b8decdb266d80c9025f66b7b152
bhagyashree samal <samalbhagyashree75@gmail.com>
2026-03-13 15:32:29 +0530

    frontend done

---
+++ frontend/src/doctor/List.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ListPage from '../components/ListPage'
+import DoctorNavbar from './DoctorNavbar'
+
+const List = () => {
+  return (
+    <div>
+        <DoctorNavbar/>
+        <ListPage/>
+    </div>
+  )
+}
+
+export default List
